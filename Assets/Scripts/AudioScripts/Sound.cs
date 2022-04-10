b80e70fba1fe13998000f390d8fc7ab058c56fa4
Mindicree <tsumararaiko@gmail.com>
2022-04-10 16:44:31 -0400

    added all sounds to audio engine

--- Assets/Scripts/AudioScripts/Sound.cs
+++ Assets/Scripts/AudioScripts/Sound.cs
@@ -10,7 +10,7 @@ public class Sound
     public AudioClip clip;
 
     [Range(0f, 1f)]
-    public float volume;
+    public float volume = 1f;
     
     public bool loop;
     public float start = 0;
